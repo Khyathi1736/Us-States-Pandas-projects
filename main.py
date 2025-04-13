e8ef5552c14d87b76bdb4056a5f1bd784c8e9e70
Khyathi <khyathivardhan17@gmail.com>
2025-04-13 11:27:40 +0530

    change turtle input logic

--- main.py
+++ main.py
@@ -1,50 +1,44 @@
-import turtle as t 
-import pandas as pd
-import time
+import turtle
+import pandas
 
-screen=t.Screen()
-image="blank_states_img.gif"
-# to add new shape to the turtle 
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
 screen.addshape(image)
-# after added that shape we can access that shape 
-timmy=t.Turtle()
-timmy.shape(image)
+turtle.shape(image)
 
+data = pandas.read_csv("50_US_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
 
-# to print dataframe without row index 
-# print(data.to_string(index=False))
-data=pd.read_csv("50_US_states.csv")
-all_states=data.state.to_list()
+h=turtle.Turtle()
+h.hideturtle()
+h.penup()
+h.goto(10,280)
+h.pendown()
+h.write(f"enter exit to exit!",align="center",font=("Arial",20,"normal"))
 
-w=t.Turtle()
-w.hideturtle()
-k=t.Turtle()
-k.hideturtle()
-score=0
-length=len(all_states)
-for i in range(length):
-    name=screen.textinput(title="Guess the State",prompt=f"What's another state name?   {score}/{length}").title()
-    # name=name.capitalize()
-    if score==length:
-        k.goto(0,0)
-        k.write("You guessed all states correctly")
-        break 
-      
-    elif name=="Exit":
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
         break
-    
-    elif name in all_states:
-        k.pu()
-        row=data[data["state"]==name]
-        k.goto(int(row.x),int(row.y))
-        # k.write(row.state.item(),align="center",font=("Arial",8,"normal"))
-        k.write(name,align="center",font=("Arial",10,"normal"))
-        score+=1
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
     else:
-        w.pu()
-        w.clear()
-        w.goto(10,280)
-        w.write(f"No state named {name}",align="center",font=("Arial",20,"normal"))
- 
-
-screen.exitonclick()
+        h.pu()
+        h.clear()
+        h.goto(10,280)
+        h.write(f"No state named {answer_state}",align="center",font=("Arial",20,"normal"))
